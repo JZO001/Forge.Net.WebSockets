52a7a3cde2285bc4c53edd96f56b2e773b10341b
Zoltan Juhasz <jzo@jzo.hu>
2022-12-14 21:54:55 +0100

    Initial commit

---
+++ WebSocketManager.cs
@@ -0,0 +1,77 @@
+﻿/* *********************************************************************
+ * Date: 10 Jun 2019
+ * Created by: Zoltan Juhasz
+ * E-Mail: forge@jzo.hu
+***********************************************************************/
+
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Forge.Net.WebSockets
+{
+
+    /// <summary>WebSocket manager</summary>
+    public class WebSocketManager
+    {
+
+        private readonly ConcurrentDictionary<string, WebSocket> mSockets = new ConcurrentDictionary<string, WebSocket>();
+
+        /// <summary>Initializes a new instance of the <see cref="WebSocketManager" /> class.</summary>
+        public WebSocketManager()
+        {
+        }
+
+        /// <summary>Gets all registered sockets from the store</summary>
+        /// <returns></returns>
+        public ConcurrentDictionary<string, WebSocket> GetAllSockets()
+        {
+            return mSockets;
+        }
+
+        /// <summary>Gets the socket by identifier from the store</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public WebSocket GetSocketById(string id)
+        {
+            return mSockets.FirstOrDefault(p => p.Key == id).Value;
+        }
+
+        /// <summary>Gets the identifier by socket.</summary>
+        /// <param name="socket">The socket.</param>
+        /// <returns></returns>
+        public string GetIdBySocket(WebSocket socket)
+        {
+            return mSockets.FirstOrDefault(p => p.Value == socket).Key;
+        }
+
+        /// <summary>Adds a newly connected socket.</summary>
+        /// <param name="socket">The socket.</param>
+        public void AddSocket(WebSocket socket)
+        {
+            mSockets.TryAdd(CreateSocketId(), socket);
+        }
+
+        /// <summary>Removes a socket.</summary>
+        /// <param name="id">The identifier.</param>
+        public async Task RemoveSocket(string id)
+        {
+            WebSocket socket;
+            mSockets.TryRemove(id, out socket);
+
+            await socket.CloseAsync(closeStatus: WebSocketCloseStatus.NormalClosure,
+                                    statusDescription: "Closed by the WebSocketManager",
+                                    cancellationToken: CancellationToken.None);
+        }
+
+        private string CreateSocketId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+    }
+
+}
---
+++ WebSocketManagerMiddleware.cs
@@ -0,0 +1,82 @@
+﻿/* *********************************************************************
+ * Date: 10 Jun 2019
+ * Created by: Zoltan Juhasz
+ * E-Mail: forge@jzo.hu
+***********************************************************************/
+
+#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Forge.Net.WebSockets
+{
+
+    /// <summary>MiddleWare class for WebSocket Management</summary>
+    public class WebSocketManagerMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly WebSocketHandler _webSocketHandler;
+
+        /// <summary>Initializes a new instance of the <see cref="WebSocketManagerMiddleware"/> class.</summary>
+        /// <param name="next">The next.</param>
+        /// <param name="webSocketHandler">The web socket handler.</param>
+        public WebSocketManagerMiddleware(RequestDelegate next, WebSocketHandler webSocketHandler)
+        {
+            _next = next;
+            _webSocketHandler = webSocketHandler;
+        }
+
+        /// <summary>Gets or sets the size of the receive buffer.</summary>
+        /// <value>The size of the receive buffer.</value>
+        public static int ReceiveBufferSize { get; set; } = 4096;
+
+        /// <summary>
+        ///   <para>
+        ///  Invoke for the web application. Here receives the new connections and start listening for incoming data.</para>
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest) return;
+
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+            await _webSocketHandler.OnConnected(socket);
+
+            await Receive(socket, async (result, buffer) =>
+            {
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await _webSocketHandler.OnDisconnected(socket);
+                    return;
+                }
+                else
+                {
+                    await _webSocketHandler.ReceiveAsync(socket, result, buffer);
+                    return;
+                }
+            });
+        }
+
+        private async Task Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            byte[] buffer = new byte[ReceiveBufferSize];
+            while (socket.State == WebSocketState.Open)
+            {
+                if (buffer.Length != ReceiveBufferSize) buffer = new byte[ReceiveBufferSize];
+                handleMessage(
+                    await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer), cancellationToken: CancellationToken.None),
+                    buffer
+                );
+            }
+        }
+
+    }
+
+}
+
+#endif
